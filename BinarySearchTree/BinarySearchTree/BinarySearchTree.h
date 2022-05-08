439f6030fa426c1ae84f1647c1194c8eaa26dbd9
Wangshenghao666 <2877959358@qq.com>
2022-05-08 12:11:52 +0800

    搜索二叉树1（增、查）

---
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.h
@@ -0,0 +1,120 @@
+﻿#pragma once
+
+
+//key
+template<class K>
+
+struct BSTreeNode
+{
+	BSTreeNode<K>* _left;
+	BSTreeNode<K>* _right;
+	K _key;
+
+	BSTreeNode(const K& key)
+		:_left(nullptr)
+		, _right(nullptr)
+		, _key(key)
+	{
+
+	}
+};
+
+template<class K>
+class BSTree
+{
+	typedef BSTreeNode<K> Node;
+public:
+	BSTree()
+		:_root(nullptr)
+	{
+
+	}
+	//Insert实现
+	bool Insert(const K& key)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(key);
+			return true;
+		}
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_key > key)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else if (cur->_key < key)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		cur = new Node(key);
+		if (parent->_key < key)
+		{
+			parent->_right = cur;
+		}
+		else
+		{
+			parent->_left = cur;
+		}
+		return true;
+	}
+
+	//中序遍历
+	void _InOrder(Node* root)
+	{
+		if (root == nullptr)
+		{
+			return;
+		}
+		_InOrder(root->_left);
+		cout << root->_key << " ";
+		_InOrder(root->_right);
+	}
+	void InOrder()
+	{
+		_InOrder(_root);
+	}
+	//查找
+	bool Find(const K& key)
+	{
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_key < key)
+			{
+				cur = cur->_right;
+			}
+			else if (cur->_key > key)
+			{
+				cur = cur->_right;
+			}
+			else
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+private:
+	Node* _root;
+};
+
+void TestBSTree()
+{
+	BSTree<int> t;
+	int a[] = { 5, 3, 4, 1, 7, 8, 2, 6, 0, 9 };
+	for (auto e : a)
+	{
+		t.Insert(e);
+	}
+	t.InOrder();
+}
---
+++ BinarySearchTree/BinarySearchTree/Test.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+#include"BinarySearchTree.h"
+
+int main()
+{
+	TestBSTree();
+	return 0;
+}
