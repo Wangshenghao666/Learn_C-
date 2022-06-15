80f18d5a2a357939d9af0992287025da45972f09
Wangshenghao666 <2877959358@qq.com>
2022-06-15 11:42:24 +0800

    红黑树封装set、map

---
+++ My_Set_Map/My_Set_Map/test.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <set>
+#include <map>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <functional>
+#include <assert.h>
+using namespace std;
+
+#include "RBTree.h"
+#include"mymap.h"
+#include"myset.h"
+int main()
+{
+	bit::test_set();
+	bit::test_map();
+	return 0;
+}
