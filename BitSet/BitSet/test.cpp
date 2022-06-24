f5c49b15b5b59b7cddd170a73edff7932073050a
Wangshenghao666 <2877959358@qq.com>
2022-06-24 10:55:18 +0800

    位图

---
+++ BitSet/BitSet/test.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <vector>
+#include<iostream>
+using namespace std;
+
+#include"bitset.h"
+
+int main()
+{
+	//test_bitset();
+	TestTwoBitSet();
+	return 0;
+}
