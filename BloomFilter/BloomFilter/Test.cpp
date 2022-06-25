4779bfe071397fee7227016f22abca99798cc0ae
Wangshenghao666 <2877959358@qq.com>
2022-06-25 09:48:42 +0800

    布隆过滤器

---
+++ BloomFilter/BloomFilter/Test.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+#include<bitset>//λͼ
+#include<vector>
+#include <string>
+#include <time.h>
+using namespace std;
+#include"BloomFilter.h"
+
+
+int main()
+{
+	TestBloomFilter2();
+	return 0;
+}
